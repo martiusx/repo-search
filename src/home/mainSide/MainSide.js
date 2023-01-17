5cbdbc653b72e5201a6c2b9ffb98dc80e5a0bd43
martiusx <Maciej.Cis@o2.pl>
2023-01-17 19:06:02 +0100

    nav section created - initial version

--- src/home/mainSide/MainSide.js
+++ src/home/mainSide/MainSide.js
@@ -2,10 +2,12 @@ import MainSideTable from "./MainSideTable";
 import MainSideSearch from "./MainSideSearch";
 import MainSideResultsPerPage from "./MainSideResultsPerPage";
 import AppButton from "../../components/ui/AppButton";
+import Nav from "../nav";
 
 const MainSide = function () {
   return (
     <>
+      <Nav />
       <h1>Strona Główna</h1>
       <MainSideSearch />
       <MainSideTable />
---
+++ src/home/nav/Nav.js
@@ -0,0 +1,12 @@
+import AppButton from "../../components/ui/AppButton";
+
+const Nav = function () {
+  return (
+    <nav>
+      <AppButton buttonText="Strona główna" />
+      <AppButton buttonText="Ulubione" />
+    </nav>
+  );
+};
+
+export default Nav;
